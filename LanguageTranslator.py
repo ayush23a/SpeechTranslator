cdedc6df8af7085302ff50c67a882146fcb4c321
Ayush Aman <140056729+ayush23a@users.noreply.github.com>
2023-10-24 18:32:38 +0530

    LanguageTranslator.py

--- LanguageTranslator.py
+++ LanguageTranslator.py
@@ -5,7 +5,7 @@ import pyttsx3
 
 r = sr.Recognizer()
 engine = pyttsx3.init()
-
+#Listening part - voice input (step 1 )
 with sr.Microphone() as source:
     print("Clearing the background noises...")
     r.adjust_for_ambient_noise(source,duration=1)
@@ -23,12 +23,13 @@ with sr.Microphone() as source:
     except Exception as ex:
         print(ex)
 
-    #Translation part
+    #Translation part (step 2)
     def translate():
         langinput = input("Type the language code in which you want to translate: ")
         translator= google_translator()
         translated_text = translator.translate(str(speech_text), lang_tgt= str(langinput))
         print(translated_text.text)
+    # Voice output (step 3)
         engine.say(str(translated_text))
         engine.runAndWait()
     translate()
