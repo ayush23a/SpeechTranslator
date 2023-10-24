d2accf0f9a79ba41bf5e4c8209dcbeaaa9210376
Ayush Aman <140056729+ayush23a@users.noreply.github.com>
2023-10-24 17:00:34 +0530

    Add files via upload

---
+++ LanguageTranslator.py
@@ -0,0 +1,36 @@
+import speech_recognition as sr 
+from google_trans_new import google_translator
+import pyttsx3
+
+
+r = sr.Recognizer()
+engine = pyttsx3.init()
+
+with sr.Microphone() as source:
+    print("Clearing the background noises...")
+    r.adjust_for_ambient_noise(source,duration=1)
+    #print("Speak Now!!")
+    audio = r.listen(source,timeout=1)
+    print("Done Recording")
+    try:
+        print("Recognizing...")
+        speech_text = r.recognize_google(audio,language='en')
+        print(speech_text)
+    except sr.UnknownValueError:
+        print("Could not understand. Please speak clearly")
+    except sr.RequestError:
+        print("Could not find the requested result from Google. Please try again")
+    except Exception as ex:
+        print(ex)
+
+    #Translation part
+    def translate():
+        langinput = input("Type the language code in which you want to translate: ")
+        translator= google_translator()
+        translated_text = translator.translate(str(speech_text), lang_tgt= str(langinput))
+        print(translated_text.text)
+        engine.say(str(translated_text))
+        engine.runAndWait()
+    translate()
+      
+
